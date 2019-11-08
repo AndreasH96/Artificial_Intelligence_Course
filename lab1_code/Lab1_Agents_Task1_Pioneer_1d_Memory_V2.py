00b10f2718857af8742f55ed57bf0119a14725b1
AndreasH96 <andreashaggstrom96@gmail.com>
2019-11-08 18:01:12 +0100

    working on 1d)

--- lab1_code/Lab1_Agents_Task1_Pioneer_1d_Memory_V2.py
+++ lab1_code/Lab1_Agents_Task1_Pioneer_1d_Memory_V2.py
@@ -9,7 +9,7 @@
 # should be a corresponding call to simxFinish at the end!
 import Lab1_Agents_Task1_World as World
 from random import randint
-import numpy as np
+#import numpy as np
 
 # connect to the server
 robot = World.init()
@@ -38,12 +38,14 @@ def returnToPreviousState():
         World.execute(reverseOfPreviosAction["motorspeed"], reverseOfPreviosAction["simulationTime"], reverseOfPreviosAction["clockSpeed"])
     reverseOfPreviousActions.clear()
 
-def readSensorData():
-    sensorData = dict(sensorLeft= World.getSensorReading("ultraSonicSensorLeft"), sensorRight = World.getSensorReading("ultraSonicSensorRight"))
-    if sensorData["sensorLeft"] == float("inf"):
+def readLeftAndRightSensorData():
+    ret_s, sensorLeft = World.vrep.simxGetObjectHandle(0, 'Pioneer_p3dx_ultrasonicSensor3',World.vrep.simx_opmode_oneshot_wait)
+    ret_s, sensorRight = World.vrep.simxGetObjectHandle(0, 'Pioneer_p3dx_ultrasonicSensor6',World.vrep.simx_opmode_oneshot_wait)
+    sensorData = dict(sensorLeft= getObstacleDist(sensorLeft), sensorRight = getObstacleDist(sensorRight) )
+    if sensorData["sensorLeft"] == float('inf'):
         sensorData["sensorLeft"] = 1.1
         
-    if sensorData["sensorRight"] == float("inf"):
+    if sensorData["sensorRight"] == float('inf'):
         sensorData["sensorRight"] = 1.1
     return sensorData
 
@@ -52,30 +54,32 @@ def readFrontSensors():
     for sensorIndex in range(1,9):
         ret_s, newSensor = World.vrep.simxGetObjectHandle(0, 'Pioneer_p3dx_ultrasonicSensor%s'%(sensorIndex),World.vrep.simx_opmode_oneshot_wait)
         sensors.append(dict(index= sensorIndex, distance= getObstacleDist(newSensor)))
-        #print("SENSOR TEST: %s\n %s\n"%(ret_s, newSensor))
+        
     return sensors
 
 def calcRepulsingForceVector():
     ultraSonicSensors = readFrontSensors()
     robotDirection = World.robotDirection()
     repVec = 0.0
-    repulsiveForceConstant = 0.2
+    repulsiveForceConstant = 0.5
     for sensor in ultraSonicSensors:
+        #print("SENSOR: ID %s VALUE %s"%(sensor["index"],sensor["distance"]))  
         if sensor["index"] < 5:
-            repVec += (repulsiveForceConstant * ((World.math.pi/sensor["index"]) + robotDirection )/sensor["distance"])  + World.math.pi/2
+            repVec = World.normaliseAngle( repVec + repulsiveForceConstant * ((World.math.pi)/sensor["index"]) /sensor["distance"])  
         else:
-            repVec += (repulsiveForceConstant * (robotDirection - (World.math.pi/sensor["index"]))/sensor["distance"]) + World.math.pi
-
-        print("RECVEC: ID %s VALUE %s TYPE:%s"%(sensor["index"],repVec % (World.math.pi * 2),type(repVec)))  
-    return repVec % (World.math.pi * 2)
+            repVec = World.normaliseAngle(repVec + repulsiveForceConstant * ((-World.math.pi)/sensor["index"])/sensor["distance"]) 
+        
+        #print("RECVEC: ID %s VALUE %s TYPE:%s"%(sensor["index"],repVec % (World.math.pi * 2),type(repVec)))  
+    return repVec # % (World.math.pi/2)
 
 def calcAttractingForceVector():
-    nearestBlock = World.getSensorReading("energySensor").direction + World.math.pi/2
-    print("ATTVEC:%s"%(nearestBlock))
-    return nearestBlock
+    nearestBlock = World.getSensorReading("energySensor") 
+    
+    #print("ATTVEC Direction :%s  RELATIVE X: %s, RELATIVE Y: %s"%(nearestBlock.direction, nearestBlockRelative_X, nearestBlockRelative_Y ))
+    return  World.normaliseAngle( nearestBlock.direction + World.robotDirection())
 
 def calcDirectionToMove(retractingForceVector, attractingForceVector):
-    finalForceVector = retractingForceVec + attractingForceVector
+    finalForceVector = World.normaliseAngle(retractingForceVector + attractingForceVector)
     print("NEWDIRECTION: %s" %(finalForceVector))
     return finalForceVector
 
@@ -85,14 +89,14 @@ def alignRobotToDirection(newDirection):
         if round(robotDirection,2) < round(newDirection,2):
             directionDifference =  newDirection - robotDirection
             World.execute(dict(speedLeft=directionDifference *2, speedRight=-directionDifference *2),40 ,-1)
-            reverseOfPreviousActions.append(dict(motorspeed=dict(speedLeft= 2 , speedRight= -2 ),simulationTime=0,clockSpeed=-1))
+            reverseOfPreviousActions.append(dict(motorspeed=dict(speedLeft=directionDifference *2, speedRight=-directionDifference *2),simulationTime=40,clockSpeed=-1))
 
             #print("ROBOTDIRECTION: %s   TARGETDIRECTION: %s "%(round(robotDirection,2), round(newDirection,2)))
             
         else:
             directionDifference = robotDirection - newDirection 
             World.execute(dict(speedLeft=-directionDifference *2 , speedRight=directionDifference *2 ),40,-1)
-            reverseOfPreviousActions.append(dict(motorspeed=dict(speedLeft=directionDifference * 5 , speedRight=-directionDifference * 5),simulationTime=50,clockSpeed=-1))
+            reverseOfPreviousActions.append(dict(motorspeed=dict(speedLeft=-directionDifference *2 , speedRight=directionDifference *2 ),simulationTime=40,clockSpeed=-1))
 
             #print("ROBOTDIRECTION: %s   TARGETDIRECTION: %s "%(round(robotDirection,2), round(newDirection,2)))
             
@@ -114,67 +118,47 @@ while robot: # main Control loop
     # Reasoning: figure out which action to take #
     ##############################################
     
-    energyBlocks = World.findEnergyBlocks()
-    directionToNearestEnergyBlock = energyBlocks[index][3]
-    distanceToNearestEnergyBlock = energyBlocks[index][2]
-    sensorData = dict(sensorLeft= World.getSensorReading("ultraSonicSensorLeft"), sensorRight = World.getSensorReading("ultraSonicSensorRight"))
     
     directionOfRobot= World.robotDirection()
-    # print("RobotDirection: %s, BlockDirection: %s, Difference: %s"%(directionOfRobot,directionToNearestEnergyBlock,directionOfRobot - directionToNearestEnergyBlock))
-    # print ('Time:',simulationTime,\
-    #            'ultraSonicSensorLeft:',World.getSensorReading("ultraSonicSensorLeft"),\
-    #            "ultraSonicSensorRight:", World.getSensorReading("ultraSonicSensorRight"))
-
     # if(World.getSimulationTime() - timeSinceLastPickup > 100000):
     #     index +=1
     #     timeSinceLastPickup = World.getSimulationTime()
 
     # Check if driving straight into a wall
     #currentSensorData=readSensorData()
+    leftAndRightSensorData = readLeftAndRightSensorData()
+    if (leftAndRightSensorData["sensorLeft"]) < 0.2 and  (leftAndRightSensorData["sensorRight"]  < 0.2) :
+            timeStuck +=  1
+            if timeStuck == 20:
+                returnToPreviousState()
+                timeStuck=0
+    if timeSinceLastPickup > 500000:
+        returnToPreviousState()
+        timeSinceLastPickUp = 0
 
     retractingForceVec = calcRepulsingForceVector()
-    retractingForceVec = calcAttractingForceVector()   
-
-    targetDirection = calcDirectionToMove(retractingForceVec, retractingForceVec)
-    if(round(directionOfRobot,2) != round(targetDirection,2)):
-        alignRobotToDirection(targetDirection)
-    World.execute(dict(speedLeft= 4 , speedRight=4 ),1500,-1)
-    World.collectNearestBlock()
+    attractingForceVec = calcAttractingForceVector()   
+    targetDirection = calcDirectionToMove(retractingForceVec, attractingForceVec)
     
+    alignRobotToDirection(targetDirection)
+    
+    #print(leftAndRightSensorData)
+    World.setMotorSpeeds(dict(speedLeft= leftAndRightSensorData["sensorLeft"] * 2 , speedRight=leftAndRightSensorData["sensorRight"] * 2 ) )#
+    reverseOfPreviousActions.append(dict(motorspeed=dict(speedLeft= leftAndRightSensorData["sensorLeft"] * 2 , speedRight=leftAndRightSensorData["sensorRight"] * 2 ),simulationTime=10,clockSpeed=-1))
+
+
+    nearestBlockDistance =  World.getSensorReading("energySensor").distance
+    if nearestBlockDistance < 0.5 : 
+        World.collectNearestBlock()
+        timeSinceLastPickUp = 0
+    timeSinceLastPickup += 1
     #print("MOVING TO 0: RobotDirection:%s"%(World.robotDirection()))
     #
     
     print("MOVING TO Pi RobotDirection:%s"%(World.robotDirection()))
     #for energyblock in energyBlocks:
     #   alignRobotToDirection(energyblock[3] +(World.math.pi/2))
-
-    '''if distanceToNearestEnergyBlock < 0.5:
-        motorSpeed = dict(speedLeft=0, speedRight=0)
-        World.collectNearestBlock()
-        index =0
-        timeSinceLastPickup = World.getSimulationTime()
-    elif round(directionToNearestEnergyBlock,1) < 0 :
-        World.execute(dict(speedLeft= directionToNearestEnergyBlock , speedRight=-directionToNearestEnergyBlock ),50,-1)
-        reverseOfPreviousActions.append(dict(motorspeed=dict(speedLeft= -directionToNearestEnergyBlock , speedRight=directionToNearestEnergyBlock ),simulationTime=50,clockSpeed=-1))
-       # motorSpeed = dict(speedLeft= directionToNearestEnergyBlock , speedRight=-directionToNearestEnergyBlock )
-        
-    elif round(directionToNearestEnergyBlock,1) > 0 :
-        World.execute(dict(speedLeft= directionToNearestEnergyBlock , speedRight=-directionToNearestEnergyBlock),50,-1)
-        reverseOfPreviousActions.append(dict(motorspeed=dict(speedLeft= -directionToNearestEnergyBlock , speedRight=directionToNearestEnergyBlock ),simulationTime=50,clockSpeed=-1))
-    else:
-        World.execute(dict(speedLeft= (-0.5) + currentSensorData["sensorLeft"] * 3, speedRight= (-0.5) + currentSensorData["sensorRight"] * 3 ),50,-1)
-        reverseOfPreviousActions.append(dict(motorspeed=dict(speedLeft=-currentSensorData["sensorLeft"] *3, speedRight=-currentSensorData["sensorRight"]* 3 ),simulationTime=50,clockSpeed=-1))
-       # motorSpeed = dict(speedLeft=sensorData["sensorLeft"] *3, speedRight=sensorData["sensorRight"]* 3 )
     
-
-    if currentSensorData["sensorLeft"] < 0.2 and currentSensorData["sensorRight"] < 0.2:
-        timeStuck +=1
-        print("Stucktick: %s" %(timeStuck))
-    if timeStuck == 20:
-        #index += 1
-        returnToPreviousState()
-        timeStuck=0
-    '''
     
 
 
@@ -185,13 +169,5 @@ while robot: # main Control loop
     # assign speed to the wheels
     #World.setMotorSpeeds(motorSpeed)
     # try to collect energy block (will fail if not within range)
-    if simulationTime%10000==0:
-        print ("Trying to collect a block...",World.collectNearestBlock())
-SENSORS: [{'sensorHandle': 35, 'distance': 0.7763301525494896}, 
-{'sensorHandle': 34, 'distance': inf},
-{'sensorHandle': 33, 'distance': inf}, 
-{'sensorHandle': 32, 'distance': 0.9433327344800936}, 
-{'sensorHandle': 31, 'distance': inf}, 
-{'sensorHandle': 30, 'distance': inf}, 
-{'sensorHandle': 29, 'distance': inf}, 
-{'sensorHandle': 28, 'distance': 0.9840285129662414}]
+    #if simulationTime%10000==0:
+    #     print ("Trying to collect a block...",World.collectNearestBlock())
