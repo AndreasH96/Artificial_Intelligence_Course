d50b315b0f670d0a32a72fde99207c6ce242609e
AndreasH96 <andreashaggstrom96@gmail.com>
2019-11-10 22:13:06 +0100

    Started on task 2

--- lab1_code/Lab1_Agents_Task2_PokerPlayer.py
+++ lab1_code/Lab1_Agents_Task2_PokerPlayer.py
@@ -1,5 +1,6 @@
 __author__ = 'fyt'
-
+import math
+import random
 # identify if there is one or more pairs in the hand
 
 # Rank: {2, 3, 4, 5, 6, 7, 8, 9, T, J, Q, K, A}
@@ -8,6 +9,21 @@ __author__ = 'fyt'
 # 2 example poker hands
 CurrentHand1 = ['Ad', '2s', '2c']
 CurrentHand2 = ['5s', '5c', '5d']
+
+def generateHand(occupiedCards = []):
+
+    def generateCard():
+        possibleRanks = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A']
+        possibleSuits = ['s', 'h', 'd', 'c']
+        return possibleRanks[random.randint(0,12)] + possibleSuits[random.randint(0,3)]
+
+    playerHand = []
+    for cardIndex in range(0,3):
+        generatedCard = generateCard()
+        while generatedCard in playerHand or generatedCard in occupiedCards:
+            generatedCard = generateCard()
+        playerHand.append(generatedCard)
+    return playerHand
 
 # identify hand category using IF-THEN rule
 def identifyHand(Hand_):
@@ -25,10 +41,15 @@ def analyseHand(Hand_):
     for category in functionToUse(Hand_):
         print('Category: ')
         for key in "name rank suit1 suit2".split():
-            print key,"=",category[key],
+            print (key,"=",category[key])
         print
 
-analyseHand(CurrentHand1)
+hand = generateHand()
+hand2 = generateHand(hand)
+print(hand)
+print(hand2)
+analyseHand(hand)
+analyseHand(hand2)
 
 
 
